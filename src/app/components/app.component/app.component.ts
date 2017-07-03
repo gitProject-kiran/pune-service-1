5c45f1ba4765b2204d8e89264bffc5ea3e4e3d4d
virendra.ranjane <virendra.ranjane@agiliad.com>
2017-07-03 15:27:26 +0530

    added the some extra code

--- src/app/components/app.component/app.component.ts
+++ src/app/components/app.component/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router }  from '@angular/router'; 
+
 import { IProduct } from '../../../app/common-interfaces/products';
 import { ProductService } from '../../../app/services/products.service';
 
@@ -8,7 +10,14 @@ import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>
+  template: `<div class="container">
+  <div class="jumbotron">
+    <h1>Angular 4 </h1> 
+    <p>Angular is the most popular HTML, CSS, and JS framework for developing dynamic application.</p> 
+  </div>
+  <p>This is some text.</p> 
+  <p>This is another text.</p> 
+</div><a class="button" (click)="onBack()">back to product</a>
             <ul>
                 <li><a [routerLink] = "['/Product']">Product</a></li>
                 <li><a [routerLink] = "['/Inventory']">Inventory</a></li>
@@ -18,14 +27,16 @@ import 'rxjs/add/operator/map';
 })
 
 export class AppComponent  { 
-    debugger;
-    name = 'World - Kiran'; 
     iproduct : IProduct[];
 
-    constructor(private _product : ProductService){}
+    constructor(private _product : ProductService, private _router : Router){}
     
     ngOnInit() : void {
         this._product.getproducts()
         .subscribe(iproducts => this.iproduct = iproducts);
     }
+   
+   onBack() : void{
+       this._router.navigate(['/Product']);
+   }    
 }
