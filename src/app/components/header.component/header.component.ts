0271425603f076724cbf1aef54a3e739e7296b9d
Kiran Shinde <kiran.shinde@agiliad.local>
2017-07-17 16:33:49 +0530

    fixed nav bar toggle issue

--- src/app/components/header.component/header.component.ts
+++ src/app/components/header.component/header.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
-    selector : 'header-content',
-    templateUrl : '../../../components/header.component/header.component.html',
+    selector: 'header-content',
+    templateUrl: '../../../components/header.component/header.component.html',
     styleUrls: ['../../../components/header.component/header.component.css']
 })
 
-export class headerComponent{
-    value : string = "";
+export class headerComponent {
+    @ViewChild('navbarToggler') navbarToggler: ElementRef;
+    navBarTogglerIsVisible() {
+        console.log("==>", this.navbarToggler.nativeElement);
+        return this.navbarToggler.nativeElement.offsetParent !== null;
+    }
+    collapseNav() {
+        if (this.navBarTogglerIsVisible()) {
+            this.navbarToggler.nativeElement.click();
+        }
+    }
 }
